d7eb5655e5c7310eeb49cfb42cd4e592d13e97cb
Harshita Agarwal <harshitaagarwal@Abhinavs-Air.hsd1.wa.comcast.net>
2016-06-06 17:12:31 -0700

    names

---
+++ Names.py
@@ -0,0 +1,19 @@
+users = {
+ 'Students': [ 
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for key, data in users.items():
+     print key
+     seq = 1
+     for val in data:
+          print seq, "-", val["first_name"], val["last_name"], "-", len(val["first_name"])+len(val["last_name"])
+          seq += 1
